e4c47cd990cd3d3f9916f60027c298ed4cdfb9d0
rx7 fc3s <jacalanaaronrey@gmail.com>
2025-09-10 01:21:30 +0800

    changed design

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,39 +1,501 @@
 ﻿namespace CS365___Calculator
 {
-    partial class Form1
-    {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
+	partial class Form1
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
 
-        #region Windows Form Designer generated code
+		#region Windows Form Designer generated code
 
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
-        }
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			btnOne = new Button();
+			btnTwo = new Button();
+			btnThree = new Button();
+			btnFour = new Button();
+			btnFive = new Button();
+			btnSix = new Button();
+			btnSeven = new Button();
+			btnEight = new Button();
+			btnNine = new Button();
+			btnZero = new Button();
+			btnAdd = new Button();
+			btnSubtract = new Button();
+			btnMultiply = new Button();
+			btnDivide = new Button();
+			btnEquals = new Button();
+			btnDecimal = new Button();
+			btnPercent = new Button();
+			btnBackspace = new Button();
+			btnClearEntry = new Button();
+			btnClear = new Button();
+			btnReciprocal = new Button();
+			btnPower = new Button();
+			btnNthRoot = new Button();
+			displayBox = new RichTextBox();
+			historyBox = new RichTextBox();
+			SuspendLayout();
+			// 
+			// btnOne
+			// 
+			btnOne.BackColor = Color.FromArgb(59, 59, 59);
+			btnOne.FlatAppearance.BorderSize = 0;
+			btnOne.FlatStyle = FlatStyle.Flat;
+			btnOne.Font = new Font("Consolas", 12F, FontStyle.Bold);
+			btnOne.ForeColor = Color.White;
+			btnOne.Location = new Point(5, 287);
+			btnOne.Margin = new Padding(2);
+			btnOne.Name = "btnOne";
+			btnOne.Size = new Size(80, 60);
+			btnOne.TabIndex = 0;
+			btnOne.Text = "1";
+			btnOne.UseVisualStyleBackColor = false;
+			// 
+			// btnTwo
+			// 
+			btnTwo.BackColor = Color.FromArgb(59, 59, 59);
+			btnTwo.FlatAppearance.BorderSize = 0;
+			btnTwo.FlatStyle = FlatStyle.Flat;
+			btnTwo.Font = new Font("Consolas", 12F, FontStyle.Bold);
+			btnTwo.ForeColor = Color.White;
+			btnTwo.Location = new Point(89, 287);
+			btnTwo.Margin = new Padding(2);
+			btnTwo.Name = "btnTwo";
+			btnTwo.Size = new Size(80, 60);
+			btnTwo.TabIndex = 7;
+			btnTwo.Text = "2";
+			btnTwo.UseVisualStyleBackColor = false;
+			// 
+			// btnThree
+			// 
+			btnThree.BackColor = Color.FromArgb(59, 59, 59);
+			btnThree.FlatAppearance.BorderSize = 0;
+			btnThree.FlatStyle = FlatStyle.Flat;
+			btnThree.Font = new Font("Consolas", 12F, FontStyle.Bold);
+			btnThree.ForeColor = Color.White;
+			btnThree.Location = new Point(173, 287);
+			btnThree.Margin = new Padding(2);
+			btnThree.Name = "btnThree";
+			btnThree.Size = new Size(80, 60);
+			btnThree.TabIndex = 6;
+			btnThree.Text = "3";
+			btnThree.UseVisualStyleBackColor = false;
+			// 
+			// btnFour
+			// 
+			btnFour.BackColor = Color.FromArgb(59, 59, 59);
+			btnFour.FlatAppearance.BorderSize = 0;
+			btnFour.FlatStyle = FlatStyle.Flat;
+			btnFour.Font = new Font("Consolas", 12F, FontStyle.Bold);
+			btnFour.ForeColor = Color.White;
+			btnFour.Location = new Point(5, 351);
+			btnFour.Margin = new Padding(2);
+			btnFour.Name = "btnFour";
+			btnFour.Size = new Size(80, 60);
+			btnFour.TabIndex = 8;
+			btnFour.Text = "4";
+			btnFour.UseVisualStyleBackColor = false;
+			// 
+			// btnFive
+			// 
+			btnFive.BackColor = Color.FromArgb(59, 59, 59);
+			btnFive.FlatAppearance.BorderSize = 0;
+			btnFive.FlatStyle = FlatStyle.Flat;
+			btnFive.Font = new Font("Consolas", 12F, FontStyle.Bold);
+			btnFive.ForeColor = Color.White;
+			btnFive.Location = new Point(89, 351);
+			btnFive.Margin = new Padding(2);
+			btnFive.Name = "btnFive";
+			btnFive.Size = new Size(80, 60);
+			btnFive.TabIndex = 10;
+			btnFive.Text = "5";
+			btnFive.UseVisualStyleBackColor = false;
+			// 
+			// btnSix
+			// 
+			btnSix.BackColor = Color.FromArgb(59, 59, 59);
+			btnSix.FlatAppearance.BorderSize = 0;
+			btnSix.FlatStyle = FlatStyle.Flat;
+			btnSix.Font = new Font("Consolas", 12F, FontStyle.Bold);
+			btnSix.ForeColor = Color.White;
+			btnSix.Location = new Point(173, 351);
+			btnSix.Margin = new Padding(2);
+			btnSix.Name = "btnSix";
+			btnSix.Size = new Size(80, 60);
+			btnSix.TabIndex = 9;
+			btnSix.Text = "6";
+			btnSix.UseVisualStyleBackColor = false;
+			// 
+			// btnSeven
+			// 
+			btnSeven.BackColor = Color.FromArgb(59, 59, 59);
+			btnSeven.FlatAppearance.BorderSize = 0;
+			btnSeven.FlatStyle = FlatStyle.Flat;
+			btnSeven.Font = new Font("Consolas", 12F, FontStyle.Bold);
+			btnSeven.ForeColor = Color.White;
+			btnSeven.Location = new Point(5, 415);
+			btnSeven.Margin = new Padding(2);
+			btnSeven.Name = "btnSeven";
+			btnSeven.Size = new Size(80, 60);
+			btnSeven.TabIndex = 11;
+			btnSeven.Text = "7";
+			btnSeven.UseVisualStyleBackColor = false;
+			// 
+			// btnEight
+			// 
+			btnEight.BackColor = Color.FromArgb(59, 59, 59);
+			btnEight.FlatAppearance.BorderSize = 0;
+			btnEight.FlatStyle = FlatStyle.Flat;
+			btnEight.Font = new Font("Consolas", 12F, FontStyle.Bold);
+			btnEight.ForeColor = Color.White;
+			btnEight.Location = new Point(89, 415);
+			btnEight.Margin = new Padding(2);
+			btnEight.Name = "btnEight";
+			btnEight.Size = new Size(80, 60);
+			btnEight.TabIndex = 13;
+			btnEight.Text = "8";
+			btnEight.UseVisualStyleBackColor = false;
+			// 
+			// btnNine
+			// 
+			btnNine.BackColor = Color.FromArgb(59, 59, 59);
+			btnNine.FlatAppearance.BorderSize = 0;
+			btnNine.FlatStyle = FlatStyle.Flat;
+			btnNine.Font = new Font("Consolas", 12F, FontStyle.Bold);
+			btnNine.ForeColor = Color.White;
+			btnNine.Location = new Point(173, 415);
+			btnNine.Margin = new Padding(2);
+			btnNine.Name = "btnNine";
+			btnNine.Size = new Size(80, 60);
+			btnNine.TabIndex = 12;
+			btnNine.Text = "9";
+			btnNine.UseVisualStyleBackColor = false;
+			// 
+			// btnZero
+			// 
+			btnZero.BackColor = Color.FromArgb(59, 59, 59);
+			btnZero.FlatAppearance.BorderSize = 0;
+			btnZero.FlatStyle = FlatStyle.Flat;
+			btnZero.Font = new Font("Consolas", 12F, FontStyle.Bold);
+			btnZero.ForeColor = Color.White;
+			btnZero.Location = new Point(5, 479);
+			btnZero.Margin = new Padding(2);
+			btnZero.Name = "btnZero";
+			btnZero.Size = new Size(164, 60);
+			btnZero.TabIndex = 19;
+			btnZero.Text = "0";
+			btnZero.UseVisualStyleBackColor = false;
+			// 
+			// btnAdd
+			// 
+			btnAdd.BackColor = Color.FromArgb(57, 34, 77);
+			btnAdd.FlatAppearance.BorderSize = 0;
+			btnAdd.FlatStyle = FlatStyle.Flat;
+			btnAdd.Font = new Font("Consolas", 14F, FontStyle.Bold);
+			btnAdd.ForeColor = Color.FromArgb(152, 43, 237);
+			btnAdd.Location = new Point(257, 415);
+			btnAdd.Margin = new Padding(2);
+			btnAdd.Name = "btnAdd";
+			btnAdd.Size = new Size(80, 60);
+			btnAdd.TabIndex = 20;
+			btnAdd.Text = "+";
+			btnAdd.UseVisualStyleBackColor = false;
+			// 
+			// btnSubtract
+			// 
+			btnSubtract.BackColor = Color.FromArgb(57, 34, 77);
+			btnSubtract.FlatAppearance.BorderSize = 0;
+			btnSubtract.FlatStyle = FlatStyle.Flat;
+			btnSubtract.Font = new Font("Consolas", 14F, FontStyle.Bold);
+			btnSubtract.ForeColor = Color.FromArgb(152, 43, 237);
+			btnSubtract.Location = new Point(257, 351);
+			btnSubtract.Margin = new Padding(2);
+			btnSubtract.Name = "btnSubtract";
+			btnSubtract.Size = new Size(80, 60);
+			btnSubtract.TabIndex = 16;
+			btnSubtract.Text = "−";
+			btnSubtract.UseVisualStyleBackColor = false;
+			// 
+			// btnMultiply
+			// 
+			btnMultiply.BackColor = Color.FromArgb(57, 34, 77);
+			btnMultiply.FlatAppearance.BorderSize = 0;
+			btnMultiply.FlatStyle = FlatStyle.Flat;
+			btnMultiply.Font = new Font("Consolas", 14F, FontStyle.Bold);
+			btnMultiply.ForeColor = Color.FromArgb(152, 43, 237);
+			btnMultiply.Location = new Point(257, 287);
+			btnMultiply.Margin = new Padding(2);
+			btnMultiply.Name = "btnMultiply";
+			btnMultiply.Size = new Size(80, 60);
+			btnMultiply.TabIndex = 15;
+			btnMultiply.Text = "×";
+			btnMultiply.UseVisualStyleBackColor = false;
+			// 
+			// btnDivide
+			// 
+			btnDivide.BackColor = Color.FromArgb(57, 34, 77);
+			btnDivide.FlatAppearance.BorderSize = 0;
+			btnDivide.FlatStyle = FlatStyle.Flat;
+			btnDivide.Font = new Font("Consolas", 14F, FontStyle.Bold);
+			btnDivide.ForeColor = Color.FromArgb(152, 43, 237);
+			btnDivide.Location = new Point(257, 223);
+			btnDivide.Margin = new Padding(2);
+			btnDivide.Name = "btnDivide";
+			btnDivide.Size = new Size(80, 60);
+			btnDivide.TabIndex = 25;
+			btnDivide.Text = "÷";
+			btnDivide.UseVisualStyleBackColor = false;
+			// 
+			// btnEquals
+			// 
+			btnEquals.BackColor = Color.FromArgb(152, 43, 237);
+			btnEquals.FlatAppearance.BorderSize = 0;
+			btnEquals.FlatStyle = FlatStyle.Flat;
+			btnEquals.Font = new Font("Consolas", 14F, FontStyle.Bold);
+			btnEquals.Location = new Point(257, 479);
+			btnEquals.Margin = new Padding(2);
+			btnEquals.Name = "btnEquals";
+			btnEquals.Size = new Size(80, 60);
+			btnEquals.TabIndex = 29;
+			btnEquals.Text = "=";
+			btnEquals.UseVisualStyleBackColor = false;
+			// 
+			// btnDecimal
+			// 
+			btnDecimal.BackColor = Color.FromArgb(59, 59, 59);
+			btnDecimal.FlatAppearance.BorderSize = 0;
+			btnDecimal.FlatStyle = FlatStyle.Flat;
+			btnDecimal.Font = new Font("Consolas", 12F, FontStyle.Bold);
+			btnDecimal.ForeColor = Color.White;
+			btnDecimal.Location = new Point(173, 479);
+			btnDecimal.Margin = new Padding(2);
+			btnDecimal.Name = "btnDecimal";
+			btnDecimal.Size = new Size(80, 60);
+			btnDecimal.TabIndex = 18;
+			btnDecimal.Text = ".";
+			btnDecimal.UseVisualStyleBackColor = false;
+			// 
+			// btnPercent
+			// 
+			btnPercent.BackColor = Color.FromArgb(60, 54, 45);
+			btnPercent.FlatAppearance.BorderSize = 0;
+			btnPercent.FlatStyle = FlatStyle.Flat;
+			btnPercent.Font = new Font("Consolas", 12F, FontStyle.Bold);
+			btnPercent.ForeColor = Color.FromArgb(255, 138, 0);
+			btnPercent.Location = new Point(5, 159);
+			btnPercent.Margin = new Padding(2);
+			btnPercent.Name = "btnPercent";
+			btnPercent.Size = new Size(80, 60);
+			btnPercent.TabIndex = 22;
+			btnPercent.Text = "%";
+			btnPercent.UseVisualStyleBackColor = false;
+			// 
+			// btnBackspace
+			// 
+			btnBackspace.BackColor = Color.FromArgb(255, 138, 0);
+			btnBackspace.FlatAppearance.BorderSize = 0;
+			btnBackspace.FlatStyle = FlatStyle.Flat;
+			btnBackspace.Font = new Font("Consolas", 18F, FontStyle.Bold);
+			btnBackspace.Location = new Point(257, 159);
+			btnBackspace.Margin = new Padding(2);
+			btnBackspace.Name = "btnBackspace";
+			btnBackspace.Size = new Size(80, 60);
+			btnBackspace.TabIndex = 14;
+			btnBackspace.Text = "←";
+			btnBackspace.UseVisualStyleBackColor = false;
+			btnBackspace.Click += btnBackspace_Click;
+			// 
+			// btnClearEntry
+			// 
+			btnClearEntry.BackColor = Color.FromArgb(60, 54, 45);
+			btnClearEntry.FlatAppearance.BorderSize = 0;
+			btnClearEntry.FlatStyle = FlatStyle.Flat;
+			btnClearEntry.Font = new Font("Consolas", 12F, FontStyle.Bold);
+			btnClearEntry.ForeColor = Color.FromArgb(255, 138, 0);
+			btnClearEntry.Location = new Point(89, 159);
+			btnClearEntry.Margin = new Padding(2);
+			btnClearEntry.Name = "btnClearEntry";
+			btnClearEntry.Size = new Size(80, 60);
+			btnClearEntry.TabIndex = 24;
+			btnClearEntry.Text = "CE";
+			btnClearEntry.UseVisualStyleBackColor = false;
+			// 
+			// btnClear
+			// 
+			btnClear.BackColor = Color.FromArgb(60, 54, 45);
+			btnClear.FlatAppearance.BorderSize = 0;
+			btnClear.FlatStyle = FlatStyle.Flat;
+			btnClear.Font = new Font("Consolas", 12F, FontStyle.Bold);
+			btnClear.ForeColor = Color.FromArgb(255, 138, 0);
+			btnClear.Location = new Point(173, 159);
+			btnClear.Margin = new Padding(2);
+			btnClear.Name = "btnClear";
+			btnClear.Size = new Size(80, 60);
+			btnClear.TabIndex = 23;
+			btnClear.Text = "C";
+			btnClear.UseVisualStyleBackColor = false;
+			// 
+			// btnReciprocal
+			// 
+			btnReciprocal.BackColor = Color.FromArgb(60, 54, 45);
+			btnReciprocal.FlatAppearance.BorderSize = 0;
+			btnReciprocal.FlatStyle = FlatStyle.Flat;
+			btnReciprocal.Font = new Font("Consolas", 12F, FontStyle.Bold);
+			btnReciprocal.ForeColor = Color.FromArgb(255, 138, 0);
+			btnReciprocal.Location = new Point(5, 223);
+			btnReciprocal.Margin = new Padding(2);
+			btnReciprocal.Name = "btnReciprocal";
+			btnReciprocal.Size = new Size(80, 60);
+			btnReciprocal.TabIndex = 26;
+			btnReciprocal.Text = "ʸ⁄ₓ";
+			btnReciprocal.UseVisualStyleBackColor = false;
+			// 
+			// btnPower
+			// 
+			btnPower.BackColor = Color.FromArgb(60, 54, 45);
+			btnPower.FlatAppearance.BorderSize = 0;
+			btnPower.FlatStyle = FlatStyle.Flat;
+			btnPower.Font = new Font("Consolas", 12F, FontStyle.Bold);
+			btnPower.ForeColor = Color.FromArgb(255, 138, 0);
+			btnPower.Location = new Point(89, 223);
+			btnPower.Margin = new Padding(2);
+			btnPower.Name = "btnPower";
+			btnPower.Size = new Size(80, 60);
+			btnPower.TabIndex = 28;
+			btnPower.Text = "Xⁿ";
+			btnPower.UseVisualStyleBackColor = false;
+			// 
+			// btnNthRoot
+			// 
+			btnNthRoot.BackColor = Color.FromArgb(60, 54, 45);
+			btnNthRoot.FlatAppearance.BorderSize = 0;
+			btnNthRoot.FlatStyle = FlatStyle.Flat;
+			btnNthRoot.Font = new Font("Consolas", 12F, FontStyle.Bold);
+			btnNthRoot.ForeColor = Color.FromArgb(255, 138, 0);
+			btnNthRoot.Location = new Point(173, 223);
+			btnNthRoot.Margin = new Padding(2);
+			btnNthRoot.Name = "btnNthRoot";
+			btnNthRoot.Size = new Size(80, 60);
+			btnNthRoot.TabIndex = 27;
+			btnNthRoot.Text = "ⁿ√";
+			btnNthRoot.UseVisualStyleBackColor = false;
+			// 
+			// displayBox
+			// 
+			displayBox.BackColor = Color.FromArgb(32, 32, 32);
+			displayBox.BorderStyle = BorderStyle.None;
+			displayBox.Location = new Point(5, 53);
+			displayBox.Margin = new Padding(3, 0, 3, 3);
+			displayBox.Name = "displayBox";
+			displayBox.Size = new Size(326, 102);
+			displayBox.TabIndex = 5;
+			displayBox.Text = "";
+			displayBox.TextChanged += displayBox_TextChanged;
+			// 
+			// historyBox
+			// 
+			historyBox.BackColor = Color.FromArgb(32, 32, 32);
+			historyBox.BorderStyle = BorderStyle.None;
+			historyBox.Location = new Point(5, 8);
+			historyBox.Margin = new Padding(3, 3, 3, 0);
+			historyBox.Name = "historyBox";
+			historyBox.Size = new Size(326, 45);
+			historyBox.TabIndex = 30;
+			historyBox.Text = "";
+			// 
+			// Form1
+			// 
+			AutoScaleDimensions = new SizeF(7F, 15F);
+			AutoScaleMode = AutoScaleMode.Font;
+			BackColor = Color.FromArgb(32, 32, 32);
+			ClientSize = new Size(344, 546);
+			Controls.Add(historyBox);
+			Controls.Add(btnEquals);
+			Controls.Add(btnPower);
+			Controls.Add(btnNthRoot);
+			Controls.Add(btnReciprocal);
+			Controls.Add(btnDivide);
+			Controls.Add(btnClearEntry);
+			Controls.Add(btnClear);
+			Controls.Add(btnPercent);
+			Controls.Add(btnAdd);
+			Controls.Add(btnZero);
+			Controls.Add(btnDecimal);
+			Controls.Add(btnSubtract);
+			Controls.Add(btnMultiply);
+			Controls.Add(btnBackspace);
+			Controls.Add(btnEight);
+			Controls.Add(btnNine);
+			Controls.Add(btnSeven);
+			Controls.Add(btnFive);
+			Controls.Add(btnSix);
+			Controls.Add(btnFour);
+			Controls.Add(btnTwo);
+			Controls.Add(btnThree);
+			Controls.Add(displayBox);
+			Controls.Add(btnOne);
+			FormBorderStyle = FormBorderStyle.None;
+			Name = "Form1";
+			Padding = new Padding(5);
+			Text = "Form1";
+			Load += Form1_Load;
+			ResumeLayout(false);
+		}
 
-        #endregion
-    }
+		#endregion
+
+		// Display elements
+		private RichTextBox displayBox;
+		private RichTextBox historyBox;
+
+		// Number buttons
+		private Button btnZero;
+		private Button btnOne;
+		private Button btnTwo;
+		private Button btnThree;
+		private Button btnFour;
+		private Button btnFive;
+		private Button btnSix;
+		private Button btnSeven;
+		private Button btnEight;
+		private Button btnNine;
+
+		// Basic operation buttons
+		private Button btnAdd;
+		private Button btnSubtract;
+		private Button btnMultiply;
+		private Button btnDivide;
+		private Button btnEquals;
+
+		// Function buttons
+		private Button btnDecimal;
+		private Button btnPercent;
+		private Button btnBackspace;
+		private Button btnClearEntry;
+		private Button btnClear;
+
+		// Advanced operation buttons
+		private Button btnReciprocal;
+		private Button btnPower;
+		private Button btnNthRoot;
+	}
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,30 @@
-namespace CS365___Calculator
+﻿namespace CS365___Calculator
 {
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
-            InitializeComponent();
-        }
-    }
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void btnBackspace_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void displayBox_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void btnZero_Click(object sender, EventArgs e)
+		{
+
+		}
+	}
 }
